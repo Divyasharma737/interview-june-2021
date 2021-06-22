520a4cd246409c0e2beec659c408d689f708b5f4
Divyasharma737 <63069149+Divyasharma737@users.noreply.github.com>
2021-06-22 12:53:44 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,39 @@
-package exam;
-
-public class CountCombinations {
-
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
-
+import java.util.*;
+ class Main
+{  
+ public static void main(String args[])
+ {
+         Scanner sc = new Scanner(System.in);
+         int p,n,c;
+ 
+         System.out.println("Enter the number of elements:") ;
+         n = sc.nextInt();
+         int[] a = new int[n];
+ 
+         System.out.println("Enter the elements") ;
+         for(p=0;p<n;p++)
+       { 
+          a[p] = sc.nextInt();
+       }
+ 
+     	   System.out.println("Original array");
+         for(p=0;p<n;p++)
+       {
+         System.out.print(a[p]+" ");
+       }
+ 
+     /* shifting array elements */
+        c=a[n-1];
+        for(p=n-1;p>0;p--)
+       {
+          a[p]=a[p-1];
+        }
+         a[0]=c;
+ 
+        System.out.println("\nNew array after rotating by one postion in the right direction");
+        for(p=0;p<n;p++)
+       {
+         System.out.print(a[p]+" ");
+        }
+ } 
 }
